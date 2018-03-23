1a1e49e3741d29d351a026e31508beb942c76d1b
AKD47 <van83horn@gmail.com>
2018-03-23 18:01:05 +0300

    main slider css

--- app/js/script.js
+++ app/js/script.js
@@ -21,4 +21,39 @@ $(document).ready(function () {
     });
     /*close*/
 
+    /*main-slider*/
+    $('.main-slider__top').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.main-slider__for'
+    });
+    $('.main-slider__for').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.main-slider__top',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+    /*close*/
 });
