17ed2ee7a46238e1035b124fb1ee9e9637812299
AKD47 <van83horn@gmail.com>
2018-04-03 11:16:21 +0300

    ajax form

--- app/js/script.js
+++ app/js/script.js
@@ -257,7 +257,6 @@ $(document).ready(function () {
     /*close*/
 
     /*slider title animation*/
-
     if ($(this).width() > 1024) {
         $('.main-slider__top').find('.main-slider__top--title').delay(1200).animate({
                 opacity: 1,
@@ -265,7 +264,6 @@ $(document).ready(function () {
             }, 'slow'
         );
     }
-    /*close*/
 
     $(window).scroll(function(){
         var about_slider = $('.about__slider-top').offset().top - $(window).height(),
@@ -293,4 +291,74 @@ $(document).ready(function () {
             }
         }
     });
+    /*close*/
+
+    /*contacts form*/
+    $('#contact-name, #contact-email, #contact-phone, #contact-text').unbind().blur(function () {
+
+        var id = $(this).attr('id');
+        var val = $(this).val();
+
+        switch (id) {
+            case 'contact-name':
+                var rv_name = /^[a-zA-Zа-яА-Я]+$/;
+                if (val.length > 2 && val != '' && rv_name.test(val)) {
+                    $(this).removeClass('error').addClass('not_error');
+                } else {
+                    $(this).removeClass('not_error').addClass('error');
+                }
+                break;
+
+            case 'contact-email':
+                var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+                if (val != '' && rv_email.test(val)) {
+                    $(this).removeClass('error').addClass('not_error');
+                } else {
+                    $(this).removeClass('not_error').addClass('error');
+                }
+                break;
+
+            case 'contact-phone':
+                var rv_phone = /^([0-9_-])+/;
+                if (val != '' && rv_phone.test(val)) {
+                    $(this).removeClass('error').addClass('not_error');
+                } else {
+                    $(this).removeClass('not_error').addClass('error');
+                }
+                break;
+
+            case 'contact-text':
+                if (val != '' && val.length < 5000) {
+                    $(this).removeClass('error').addClass('not_error');
+                } else {
+                    $(this).removeClass('not_error').addClass('error');
+                }
+                break;
+
+        } // end switch(...)
+
+    }); // end blur()
+    $('#contact-form').submit(function (event) {
+        event.preventDefault();
+        var name = $('#contact-name').val(),
+            mail = $('#contact-email').val(),
+            phone = $('#contact-phone').val(),
+            message = $('#contact-text').val();
+        $.ajax({
+            url: myajax.url,
+            type: "POST",
+            data: {
+                action: 'contactform',
+                name: name,
+                mail: mail,
+                phone: phone,
+                message: message
+            },
+            success: function (data) {
+                $('#contact-form input, textarea').val('').removeClass('error, not_error').text('');
+            }
+        }); // end ajax({...})
+        return false;
+    }); // end submit()
+    /*close*/
 });
